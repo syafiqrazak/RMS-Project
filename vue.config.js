4c564b35cd9e67376791c74a369856567645674b
syafiqrazak <47788213+syafiqrazak@users.noreply.github.com>
2019-11-12 00:05:44 +0800

    Update

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,9 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000"
+        // target: "http://127.0.0.1:3000"
         // target: "http://localhost:3333"
-        // target: "http://192.168.193.236:3000"
+        target: "http://192.168.193.236:3000"
       }
     }
   }
