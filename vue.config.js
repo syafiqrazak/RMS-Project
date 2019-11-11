39a77709f1050a4663687a407c4d3df77660f2c2
syafiqrazak <47788213+syafiqrazak@users.noreply.github.com>
2019-11-11 23:06:00 +0800

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
-import path from "path";
+const path = require("path");
 
-export const devServer = {
-  proxy: {
-    "/api": {
-      // target: "http://localhost:3000"
-      target: "http://localhost:3333"
-      // target: "http://192.168.193.236:3000"
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        // target: "http://localhost:3000"
+        target: "http://localhost:3333"
+        // target: "http://192.168.193.236:3000"
+      }
     }
   }
 };
